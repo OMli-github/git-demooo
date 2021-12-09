13dc543d1f0a4634b932311508815f67cee91c77
OMli <1256940@qq.com>
2021-12-10 01:17:13 +0800

    four commit

--- src/main/java/com/atguigu/springcloud/demo.java
+++ src/main/java/com/atguigu/springcloud/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("git ok");
         System.out.println("git ok2");
+        System.out.println("git ok3");
     }
 }

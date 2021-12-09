700819edee8dc51f7e676d44bf6bc66b08a4fd39
OMli <1256940@qq.com>
2021-12-10 01:16:58 +0800

    third commit

--- src/main/java/com/atguigu/springcloud/demo.java
+++ src/main/java/com/atguigu/springcloud/demo.java
@@ -3,5 +3,6 @@ package com.atguigu.springcloud;
 public class demo {
     public static void main(String[] args) {
         System.out.println("git ok");
+        System.out.println("git ok2");
     }
 }

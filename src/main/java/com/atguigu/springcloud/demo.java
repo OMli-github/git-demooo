629b368c20da6d03b55ff853ba40c803447537e6
OMli <1256940@qq.com>
2021-12-10 08:49:43 +0800

    master commit

--- src/main/java/com/atguigu/springcloud/demo.java
+++ src/main/java/com/atguigu/springcloud/demo.java
@@ -5,5 +5,6 @@ public class demo {
         System.out.println("git ok");
         System.out.println("git ok2");
         System.out.println("git ok3");
+        System.out.println("git ok-master");
     }
 }
